9634b9d5597ea58beddddeea24829d4daea0d712
receptuna <receptuna@agersoft.com>
2016-11-28 14:44:17 +0300

    SharpConfig ayar kütüphanesi eklendi.

---
+++ Devager/SharpConfig/IgnoreAttribute.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace Devager.SharpConfig
+{
+    /// <summary>
+    /// Represents an attribute that tells SharpConfig to
+    /// ignore the subject this attribute is applied to.
+    /// For example, if this attribute is applied to a property
+    /// of a type, that property will be ignored when creating
+    /// sections from objects and vice versa.
+    /// </summary>
+    public sealed class IgnoreAttribute : Attribute
+    { }
+}
---
+++ Devager/SharpConfig/ParserException.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace Devager.SharpConfig
+{
+    /// <summary>
+    /// Represents an error that occurred during
+    /// the configuration parsing stage.
+    /// </summary>
+    [Serializable]
+    public sealed class ParserException : Exception
+    {
+        internal ParserException(string message, int line)
+            : base(string.Format("Line {0}: {1}", line, message))
+        { }
+    }
+}
