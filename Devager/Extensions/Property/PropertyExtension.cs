5d181416dce05273b957561db00885d716f871e4
Osman ALTAY <osman_altay@hotmail.com>
2017-05-05 12:41:42 +0300

    PropertyExtension eklendi.

---
+++ Devager/Extensions/Property/PropertyExtension.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Devager.Extensions
+{
+    public static class PropertyExtension
+    {
+        public static void SetPropertyValue(this object obj, string propName, object value)
+        {
+            obj.GetType().GetProperty(propName).SetValue(obj, value, null);
+        }
+    }
+}
